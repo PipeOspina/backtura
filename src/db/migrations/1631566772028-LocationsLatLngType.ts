749eb364a2f2316a601e1b4a9e65544a2a300a21
Felipe Ospina <pipe.98.oh@gmail.com>
2021-09-13 17:54:32 -0500

    feat: some migrations

--- src/db/migrations/1631566772028-LocationsLatLngType.ts
+++ src/db/migrations/1631566772028-LocationsLatLngType.ts
@@ -1,18 +1,29 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class LocationsLatLngType1631566772028 implements MigrationInterface {
-    name = 'LocationsLatLngType1631566772028'
+    name = 'LocationsLatLngType1631566772028';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`Culthurum\`.\`locations\` DROP COLUMN \`point\``);
-        await queryRunner.query(`ALTER TABLE \`Culthurum\`.\`locations\` ADD \`latitude\` decimal NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`Culthurum\`.\`locations\` ADD \`longitude\` decimal NOT NULL`);
+        await queryRunner.query(
+            `ALTER TABLE \`Culthurum\`.\`locations\` DROP COLUMN \`point\``,
+        );
+        await queryRunner.query(
+            `ALTER TABLE \`Culthurum\`.\`locations\` ADD \`latitude\` decimal NOT NULL`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE \`Culthurum\`.\`locations\` ADD \`longitude\` decimal NOT NULL`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`Culthurum\`.\`locations\` DROP COLUMN \`longitude\``);
-        await queryRunner.query(`ALTER TABLE \`Culthurum\`.\`locations\` DROP COLUMN \`latitude\``);
-        await queryRunner.query(`ALTER TABLE \`Culthurum\`.\`locations\` ADD \`point\` point NOT NULL`);
+        await queryRunner.query(
+            `ALTER TABLE \`Culthurum\`.\`locations\` DROP COLUMN \`longitude\``,
+        );
+        await queryRunner.query(
+            `ALTER TABLE \`Culthurum\`.\`locations\` DROP COLUMN \`latitude\``,
+        );
+        await queryRunner.query(
+            `ALTER TABLE \`Culthurum\`.\`locations\` ADD \`point\` point NOT NULL`,
+        );
     }
-
 }
