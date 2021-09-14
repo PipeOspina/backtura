578fb553a6c9727c46d617ea98b8cac6bdbc3f0c
Felipe Ospina <pipe.98.oh@gmail.com>
2021-09-13 19:59:23 -0500

    fix: deleting rare env variables

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOpti
 export const databaseConfig = (): MysqlConnectionOptions => ({
     type: 'mysql',
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) || 3306,
+    port: Number(process.env.DB_PORT) || undefined,
     username: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
